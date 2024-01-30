2e0ecc677540261525353ce26f43fe977396294d
Vuong Nguyen <nguvuong@sheridancollege.ca>
2024-01-30 00:34:07 -0500

    finished Appjsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import Hero from "./components/Hero";
+import Demo from "./components/Demo";
+
+import "./App.css";
 
 const App = () => {
     return (
-        <div>App</div>
-    )
-}
+        <>
+            <main>
+                <div className='main'>
+                    <div className='gradient' />
+                </div>
+                <p>
+                    Hi
+                </p>
 
-export default App
+                <div className='app'>
+                    <Hero />
+                    <Demo />
+                </div>
+            </main>
+        </>
+
+    );
+};
+
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,jsx}"],
+  content: ["./src/**/*.{html,js,jsx}", 
+            './components/**/*.{jsx}',
+            './pages/**/*.{html,js}',
+            './index.html',],
   theme: {
     extend: {
       fontFamily: {
